af8ec6f6a9b64bca35c8b10420dde1a590517a4c
Anderson Barg <andbarg@yahoo.com.br>
2026-07-17 14:55:45 -0300

    DepartmentDao and SellerDao interfaces

---
+++ src/model/dao/DepartmentDao.java
@@ -0,0 +1,30 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Department;
+
+public interface DepartmentDao {
+	
+	public static void insert(Department obj) {
+		
+	}
+	
+	public static void update(Department obj) {
+		
+	}
+	
+	public static void deleteById(Integer id) {
+		
+	}
+	
+	public static Department findById(Integer id) {
+		return null;
+	}
+
+	public static List<Department> findALL() {
+		return null;
+	}
+
+	
+}
---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,29 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Seller;
+
+public interface SellerDao {
+
+	
+	public static void insert(Seller obj) {
+		
+	}
+	
+	public static void update(Seller obj) {
+		
+	}
+	
+	public static void deleteById(Integer id) {
+		
+	}
+	
+	public static Seller findById(Integer id) {
+		return null;
+	}
+
+	public static List<Seller> findALL() {
+		return null;
+	}
+}
