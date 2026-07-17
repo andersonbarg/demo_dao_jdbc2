0c92ee7bd05e35efebc669288a57e163d8709d90
Anderson Barg <andbarg@yahoo.com.br>
2026-07-17 12:19:30 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+	
+	void main() {
+		
+		Department obj = new Department(1, "Books");
+		
+		IO.println(obj);
+	}
+}
